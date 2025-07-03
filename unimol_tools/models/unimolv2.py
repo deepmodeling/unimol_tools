609b50b6ad6c7765d4f81c9ca8947b8d751caf8f
darkkoo <149583948+darkkoo@users.noreply.github.com>
2025-07-03 13:49:53 +0800

    Update unimolv2.py

--- unimol_tools/models/unimolv2.py
+++ unimol_tools/models/unimolv2.py
@@ -270,21 +270,22 @@ class UniMolV2Model(nn.Module):
         all_repr = x[:, :, :]  # all token repr
 
         if return_repr:
-            filtered_tensors = []
-            filtered_coords = []
-
-            for tokens, coord in zip(src_tokens, src_coord):
-                filtered_tensor = tokens[
-                    (tokens != 0) & (tokens != 1) & (tokens != 2)
-                ]  # filter out BOS(0), EOS(1), PAD(2)
-                filtered_coord = coord[(tokens != 0) & (tokens != 1) & (tokens != 2)]
-                filtered_tensors.append(filtered_tensor)
-                filtered_coords.append(filtered_coord)
-
-            lengths = [
-                len(filtered_tensor) for filtered_tensor in filtered_tensors
-            ]  # Compute the lengths of the filtered tensors
             if return_atomic_reprs:
+                filtered_tensors = []
+                filtered_coords = []
+
+                for tokens, coord in zip(src_tokens, src_coord):
+                    filtered_tensor = tokens[
+                        (tokens != 0) & (tokens != 1) & (tokens != 2)
+                    ]  # filter out BOS(0), EOS(1), PAD(2)
+                    filtered_coord = coord[(tokens != 0) & (tokens != 1) & (tokens != 2)]
+                    filtered_tensors.append(filtered_tensor)
+                    filtered_coords.append(filtered_coord)
+
+                lengths = [
+                    len(filtered_tensor) for filtered_tensor in filtered_tensors
+                ]  # Compute the lengths of the filtered tensors
+            
                 cls_atomic_reprs = []
                 atomic_symbols = []
                 for i in range(len(all_repr)):
@@ -299,7 +300,7 @@ class UniMolV2Model(nn.Module):
                     'atomic_reprs': cls_atomic_reprs,
                 }
             else:
-                return {'cls_repr': cls_repr}
+                return cls_repr
 
         logits = self.classification_head(cls_repr)
         return logits
