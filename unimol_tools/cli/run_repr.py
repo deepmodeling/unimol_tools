7bf00cf874979f98216f338df69da9c583409d6e
zhoushijia <cabc6888@gmail.com>
2025-09-03 06:26:25 +0000

    run_repr save csv

--- unimol_tools/cli/run_repr.py
+++ unimol_tools/cli/run_repr.py
@@ -9,17 +9,25 @@ from ..predictor import UniMolRepr
 @hydra.main(version_base=None, config_path="../config", config_name="repr_config")
 def main(cfg: DictConfig):
     data_path = cfg.get("data_path")
-    return_atomic_reprs = cfg.get("return_atomic_reprs", False)
     return_tensor = cfg.get("return_tensor", False)
+    return_atomic_reprs = cfg.get("return_atomic_reprs", False)
     encoder = UniMolRepr(cfg=cfg)
-    reprs = encoder.get_repr(data=data_path, return_atomic_reprs=return_atomic_reprs, return_tensor=return_tensor)
-    repr_path = cfg.get("repr_path")
+    reprs = encoder.get_repr(
+        data=data_path,
+        return_atomic_reprs=return_atomic_reprs,
+        return_tensor=return_tensor,
+    )
     save_dir = cfg.get("save_path")
-    if repr_path is None and save_dir:
-        os.makedirs(save_dir, exist_ok=True)
-        repr_path = os.path.join(save_dir, "repr.npy")
-    if repr_path:
-        np.save(repr_path, reprs)
+
+    if not return_tensor and not return_atomic_reprs:
+        if save_dir:
+            os.makedirs(save_dir, exist_ok=True)
+            np.save(os.path.join(save_dir, "repr.npy"), reprs)
+            np.savetxt(
+                os.path.join(save_dir, "repr.csv"), np.asarray(reprs), delimiter="," 
+            )
+        else:
+            print(reprs)
     else:
         print(reprs)
 
