9b21ff1f66c0e1a249f8a4270da8780ab193684d
cuiyaning <cuiyaning@dp.tech>
2026-06-15 07:36:20 +0000

    [chore] update version 0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="unimol_tools",
-    version="0.1.5",
+    version="0.1.6",
     description=(
         "unimol_tools is a Python package for property prediction with Uni-Mol in molecule, materials and protein."
     ),
